63e6686bcb1c139b0a0da35d64093e4f1c14a093
Mida Boghetich <mida@boghetich.com>
2021-03-29 11:41:37 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/midaboghetich/dynamocleanup
+
+go 1.16
+
+require github.com/aws/aws-sdk-go v1.38.7 // indirect
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+	"strings"
+
+	"fmt"
+)
+
+func main() {
+	numArgs := len(os.Args)
+	if numArgs < 2 {
+		fmt.Println(">> No args passed in")
+		os.Exit(0)
+	}
+	user := os.Args[1]
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// create the input configuration instance
+	input := &dynamodb.ListTablesInput{}
+
+	fmt.Printf("Tables:\n")
+
+	for {
+		// Get the list of tables
+		result, err := svc.ListTables(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case dynamodb.ErrCodeInternalServerError:
+					fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+
+				fmt.Println(err.Error())
+			}
+			return
+		}
+
+		for _, n := range result.TableNames {
+
+			if strings.Contains(*n, user) {
+				fmt.Println("Deleting -> " + *n)
+				deleteTable(svc, n)
+			}
+		}
+
+		input.ExclusiveStartTableName = result.LastEvaluatedTableName
+
+		if result.LastEvaluatedTableName == nil {
+			break
+		}
+	}
+
+}
+
+func deleteTable(client *dynamodb.DynamoDB, tableName *string) {
+	input := &dynamodb.DeleteTableInput{TableName: tableName}
+
+	table, err := client.DeleteTable(input)
+
+	if err != nil {
+
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(table.String())
+
+}
